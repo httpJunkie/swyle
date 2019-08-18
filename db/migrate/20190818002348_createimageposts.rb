cb11eb221a73eeba73b1226d7c13bc277e05b52b
genxhero <genxhero@users.noreply.github.com>
2019-08-17 17:27:27 -0700

    add image posts

---
+++ db/migrate/20190818002348_createimageposts.rb
@@ -0,0 +1,12 @@
+class Createimageposts < ActiveRecord::Migration[6.0]
+  def change
+      create_table :image_posts do |t|
+      t.string   :title, null: false
+      t.string :description
+      t.int :user_id, null: false
+      t.datetime :created_at, null: false
+  end
+  add_index :image_posts, :user_id
+  add_index :image_posts, :title
+  end
+end
