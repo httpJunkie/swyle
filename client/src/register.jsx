1ae39e2f77a2eb53022a497d4748048b6fcc5e01
genxhero <genxhero@users.noreply.github.com>
2019-08-28 16:24:26 -0700

    update imports in register

--- client/src/register.jsx
+++ client/src/register.jsx
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import {Redirect } from 'react-router-dom';
+import {Query, Mutation} from 'react-apollo';
 
 class Register extends Component {
     constructor(props) {
-        super(props) {
+        super(props) 
             this.state={
                 username: null,
                 email: null,
                 password: null,
                 passwordConfirm: null
             }
-        }
     }
 }
+
+export default Register;
