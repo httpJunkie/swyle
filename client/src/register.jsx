875930a87a1e913571be508c63b017865486e128
genxhero <genxhero@users.noreply.github.com>
2019-08-28 16:34:39 -0700

    Add mutation skeleton to registeR

--- client/src/register.jsx
+++ client/src/register.jsx
@@ -27,6 +27,80 @@ class Register extends Component {
             [field]: event.currentTarget.value,
         });
     }
+
+    allowOrPreventScrolling() {
+        if (this.state.errors) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'auto');
+        }
+    }
+
+    render() {
+        this.allowOrPreventScrolling();
+        return (
+            <Query query={currentUser}>
+                {({ data, error, loading }) => {
+                    if (loading) return "...Loading";
+                    if (!data.currentUser) {
+                        return (
+                            <Mutation
+                                mutation={mutation}
+                                update={(cache, { data: { signInUser } }) => {
+                                    cache.writeQuery({
+                                        query: currentUser,
+                                        data: { currentUser: signInUser.user }
+                                    });
+                                }}
+                            >
+                                {(signInUser, { loading: authenticating }) =>
+                                    authenticating ? (
+                                        "..."
+                                    ) : (
+                                            <div className="session-page" >
+                                                <form onSubmit={event => {
+                                                    event.preventDefault();
+                                                    signInUser({
+                                                        variables: {
+                                                            email: this.state.email,
+                                                            password: this.state.password
+                                                        }
+                                                    }).then(res => {
+                                                        console.log("result:", res)
+                                                        const token = res.data.signInUser.token
+                                                        if (token) {
+                                                            localStorage.setItem("mlToken", token)
+                                                        }
+
+                                                    }).then(() => {
+                                                        this.props.history.push('/')
+                                                    })
+                                                        .catch(res => {
+                                                            this.setState({ errors: res.graphQLErrors })
+                                                        })
+                                                }} className="session-form">
+                                                    <h1>Log In</h1>
+                                                    <input className="auth-field" type="text" value={this.state.email}
+                                                        onChange={this.handleFormChange('email')} placeholder="Email" />
+                                                    <input className="auth-field" type="password" value={this.state.password}
+                                                        onChange={this.handleFormChange('password')} placeholder="Password" />
+                                                    <div className="form-footer">
+                                                        <input className="submit" type="submit" />
+                                                    </div>
+                                                </form>
+                                                {this.state.errors && <ErrorsModal errors={this.state.errors} clearErrors={this.clearErrors} />}
+                                            </div>
+                                        )
+
+                                }
+                            </Mutation>
+                        );
+
+                    }
+                }}
+            </Query>
+        )
+    }
 }
 
 export default Register;
