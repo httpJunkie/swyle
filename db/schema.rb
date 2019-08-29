2487c0152f91637b30e2c372052957d20ae7641a
genxhero <genxhero@users.noreply.github.com>
2019-08-29 09:47:10 -0700

    migrate database, add comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_003652) do
+ActiveRecord::Schema.define(version: 2019_08_29_162737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2019_08_19_003652) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["title"], name: "index_articles_on_title"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body", null: false
+    t.string "user_id", null: false
+    t.string "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "image_posts", force: :cascade do |t|
