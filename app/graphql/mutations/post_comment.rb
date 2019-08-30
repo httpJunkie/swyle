16261ba2eb7c6bfee518f5a37bb93a1fcb5798c3
genxhero <genxhero@users.noreply.github.com>
2019-08-30 16:14:55 -0700

    make the comment mutation work proper.

--- app/graphql/mutations/post_comment.rb
+++ app/graphql/mutations/post_comment.rb
@@ -5,12 +5,12 @@ module Mutations
         argument :user_id, Integer, required: true
         argument :post_id, Integer, required: true
         type Types::CommentType
-        def resolve 
+        def resolve(body: nil, user_id: nil, post_id: nil, post_type: nil)
             comment = Comment.new
-            comment.body = :body 
-            comment.user_id = :user_id
-            comment.post_id = :post_id
-            comment.post_type = :post_type
+            comment.body = body 
+            comment.user_id = user_id
+            comment.post_id = post_id
+            comment.post_type = post_type
             comment.save
             comment
         end
