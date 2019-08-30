0bc1482d726198f4969054d6bf56726f45100702
genxhero <genxhero@users.noreply.github.com>
2019-08-30 10:30:42 -0700

    wrap article show in query

--- client/src/article_show.jsx
+++ client/src/article_show.jsx
@@ -4,6 +4,24 @@ import article from './queries/article';
 
 class ArticleShow extends Component {
 
+    render() {
+        console.log(this.props.data)
+        return (
+            <Query query={article}>
+                {({ loading, error, data }) => {
+                    if (loading) return <p>Loading...</p>;
+                    if (error) return <p>Error :(</p>;
+                        debugger;
+                    return (
+                        <div className="article-show-page">
+                            <h1></h1>
+                        </div>
+                    )
+                }}
+            </Query>
+        )
+    }
+
 }
 
 export default ArticleShow;
