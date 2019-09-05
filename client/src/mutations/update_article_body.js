e33ebc93980d7d956970ee8be05625c6b56a375d
genxhero <genxhero@users.noreply.github.com>
2019-09-05 13:23:40 -0700

    add client side mutation for updating article body

---
+++ client/src/mutations/update_article_body.js
@@ -0,0 +1,13 @@
+import gql from 'graphql-tag';
+
+const updateArticleBody = gql`
+    mutation updateArticleBody($id: Int!, $body: String!) {
+        updateArticleBody(id: $id, body: $body) {
+            id
+            body
+            title
+        }
+    }
+`;
+
+export default UpdateArticlebody
