b67e116a6ab490efb630a1c471dbe8f8e0d809cc
genxhero <genxhero@users.noreply.github.com>
2019-09-06 09:56:14 -0700

    same as body with title

--- client/src/article_title_edit.jsx
+++ client/src/article_title_edit.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Mutation} from 'react-apollo';
+import updateArticleTitle from './mutations/update_article_title'
 
 class ArticleTitleEdit extends Component {
     constructor(props) {
