f72d81c1dd52e10bbe1db52af97234bc9bbc0121
genxhero <genxhero@users.noreply.github.com>
2019-09-10 12:20:23 -0700

    change dashboard to map current user to state

--- client/src/user_dash.jsx
+++ client/src/user_dash.jsx
@@ -4,12 +4,24 @@ import {Redirect, withRouter} from 'react-router-dom';
 
 class UserDash extends Component {
 
+
+    constructor(props) {
+        super(props)
+        this.state={currentUser: this.props.currentUser}
+    }
+
+  componentWillReceiveProps(newProps) {
+      if (this.props.currentUser !== newProps.currentUser) {
+          this.setState({currentUser: newProps.currentUser})
+      }
+  }
+
   render() {
-      if (!this.props.currentUser) {
+      if (!this.state.currentUser) {
           return (<Redirect to="/login"/>)
       } else {
           return (
-              <h1>There is a user, his name is {this.props.currentUser.username}</h1>
+              <h1>There is a user, his name is {this.state.currentUser.username}</h1>
           )
       }
   }
