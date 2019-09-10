6bab6b3ecc0cad6d036e9d0a1bd495fe040fa5ac
genxhero <genxhero@users.noreply.github.com>
2019-09-10 14:33:30 -0700

    add posts by user to client

--- client/src/queries/posts_by_user.js
+++ client/src/queries/posts_by_user.js
@@ -1,6 +1,21 @@
 import gql from 'graphql-tag';
 
 const postsByUser = gql`
+query postsByUser($userId: Int!){
+    postsByUser(userId:$userId) {
+    __typename
+    ... on Article {
+      id
+      title
+      snippet
+    }
+    ... on Image {
+      id
+      title
+      image	
+    }
+  }
+}
 `
 
 export default postsByUser;
