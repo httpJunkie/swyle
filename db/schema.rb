d1f0e144df81d263756c246482056f72a0765196
genxhero <genxhero@users.noreply.github.com>
2019-09-12 11:21:29 -0700

    create the like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_181134) do
+ActiveRecord::Schema.define(version: 2019_09_12_181208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 2019_08_31_181134) do
     t.string "image_url"
     t.index ["title"], name: "index_image_posts_on_title"
     t.index ["user_id"], name: "index_image_posts_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
