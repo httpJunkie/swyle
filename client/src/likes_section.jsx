64cc838175428b145b7594052003979b0bee6f5e
genxhero <genxhero@users.noreply.github.com>
2019-09-13 14:19:33 -0700

    add classname to mdthumbup

--- client/src/likes_section.jsx
+++ client/src/likes_section.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { MdThumbUp, MdThumbDown} from 'react-icons/md';
 
 class LikesSection extends Component {
 
@@ -17,9 +18,13 @@ class LikesSection extends Component {
     }
 
     render() {
+        const userLikesIt = this.state.currentUser && this.props.likers.includes(this.state.currentUser.id);
         return (
             <div className="likes-section" style={{"color":"white"}}>
-                {this.state.currentUser && this.props.likers.includes(this.state.currentUser.id)  ? "You like this" : "You have no opinion on this"}
+                <div className="like-or-dislike">
+                    <MdThumbUp className={userLikesIt ? "like-thumb-yes" : "like-thumb-no" }/>
+                </div>
+                { userLikesIt ? "You like this" : "You have no opinion on this"}
             </div>
         )
     }
