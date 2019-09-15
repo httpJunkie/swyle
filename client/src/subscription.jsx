d38a6e327f27c3cddb52032ef3f6fef5960b3dd1
genxhero <genxhero@users.noreply.github.com>
2019-09-15 14:18:30 -0700

    add subscription component

---
+++ client/src/subscription.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { ArticleSubscription } from './subscriptions/article_added';
+
+const Subscription = ({ subscribeToMore }) => {
+  useEffect(() => {
+    return subscribeToMore({
+      document: ArticleSubscription,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const { articleAdded } = subscriptionData.data;
+
+        if (articleAdded) {
+          const alreadyInList = prev.articles.find(e => e.id === articleAdded.id);
+          if (alreadyInList) {
+            return prev;
+          }
+
+          return { ...prev, articles: prev.articles.concat([articleAdded]) };
+        }
+
+        return prev;
+      },
+    });
+  }, []);
+  return null;
+};
+
+export default Subscription;
