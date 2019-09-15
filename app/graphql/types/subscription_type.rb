ab452b7d826221059608f042e4bb929b85f2c310
genxhero <genxhero@users.noreply.github.com>
2019-09-15 12:55:47 -0700

    add subscription type

---
+++ app/graphql/types/subscription_type.rb
@@ -0,0 +1,7 @@
+module Types
+  class SubscriptionType < GraphQL::Schema::Object
+    field :article_added, Types::ArticleType, null: false, description: "An article was posted"
+    def article_added; end
+    end
+
+end
