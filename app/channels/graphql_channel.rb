48a242abe760ec42e49a20f7b9fd490718dcb4d1
genxhero <genxhero@users.noreply.github.com>
2019-09-15 12:59:46 -0700

    add execute to gql channel

--- app/channels/graphql_channel.rb
+++ app/channels/graphql_channel.rb
@@ -3,6 +3,19 @@ class GraphqlChannel < ApplicationCable::Channel
     @subscription_ids = []
   end
 
+  def execute(data)
+  result = execute_query(data)
+
+  payload = {
+    result: result.subscription? ? { data: nil } : result.to_h,
+    more: result.subscription?
+  }
+
+  @subscription_ids << context[:subscription_id] if result.context[:subscription_id]
+
+  transmit(payload) 
+ end
+
   def unsubscribed
   end
 end
