01023dce0e8a5eb30dab88a3c3545b181f7abfb6
genxhero <genxhero@users.noreply.github.com>
2019-09-15 18:14:36 -0700

    get redis to work?

--- client/src/subscriptions/article_added.js
+++ client/src/subscriptions/article_added.js
@@ -2,18 +2,22 @@ import gql from 'graphql-tag';
 
 const ArticleSubscription = gql`
 subscription ArticleSubscription {
-  articleAdded {
-     
-            id
-    title
-    body
-    likers
-    likeCount
-    author {
+   articleAdded { 
         id
-        username
-      }
+        title
+        body
+        likers
+        likeCount
+        author {
+            id
+            username
+        },
+    currentUser {
+      id
+      username
+      email
     }
+   }
     
 }
 `
