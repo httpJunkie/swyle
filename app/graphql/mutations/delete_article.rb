a996e64959960fba730b2fc484027f5ec3baced0
genxhero <genxhero@users.noreply.github.com>
2019-09-17 21:27:47 -0700

    remove useless comments from article destroy backend

--- app/graphql/mutations/delete_article.rb
+++ app/graphql/mutations/delete_article.rb
@@ -5,12 +5,8 @@ module Mutations
       type Types::ArticleType
   
       def resolve(id: nil)
-        #TODO: Check for context current user here, don't allow this to happen if the ids don't match
-        #Yes, it's tedious boilerplate code, but it's gotta be done.
-        
           article = Article.find(id)
           article.destroy
-          #Destroy
       end
     end
   end
