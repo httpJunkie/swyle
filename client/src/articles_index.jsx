46b5cfc7979f8e2fd2ca894222aba9f8863bdf9c
genxhero <genxhero@users.noreply.github.com>
2019-09-18 14:27:43 -0700

    remove old comments

--- client/src/articles_index.jsx
+++ client/src/articles_index.jsx
@@ -5,45 +5,6 @@ import {Link} from 'react-router-dom';
 import ArticleTags from './article_tags';
 import Subscription from './subscription';
 
-// class ArticlesIndex extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-           
-//         }
-//     }
-
-//     render() {
-//         const date = Date.now(); 
-         
-//         return (
-//             <Query query={articles}>
-//                 {({ loading, error, data, subscribeToMore }) => {
-//                     if (loading) return <p>Loading...</p>;
-//                     if (error) return <p>Error :(</p>;
-//                     const articles = data.articles;
-//                     return ( 
-//                     <div className="article-index-page"> 
-//                       <h1>Newest Articles</h1> 
-//                       {  articles.map((article) => (
-//                             <div className="article-index-card" key={`${article.id}${article.title}${date}`}>
-//                               <h2 className="article-index-title">{article.title}</h2>
-//                               <h3 className="article-index-subtitle">by {article.author.username}</h3>
-//                               <p className="article-index-snippet">{article.snippet}<Link className="article-index-show-link" to={`/articles/${article.id}`}>{"...more"}</Link></p>
-//                                 <ArticleTags tags={["lookAtThisTag", "othertag"] } />
-//                                 <h4>{article.count} Comments {article.likeCount} Likes</h4>                           
-//                             </div>
-//                         ))}
-//                         <Subscription subscribeToMore={subscribeToMore} />
-//                     </div>
-//                     )
-//                 }}
-//             </Query>
-//         );
-//     }
-// }
-
-
 const ArticlesIndex = () => {
         const date = Date.now();
         return (
