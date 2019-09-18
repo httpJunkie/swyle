596b6e71435170d3f9eaa2e420c26d7929258fac
genxhero <genxhero@users.noreply.github.com>
2019-09-18 11:51:32 -0700

    add advert greeting

--- client/src/adverts.jsx
+++ client/src/adverts.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Adverts = () => {
     return (
         <div className="adverts-page">
+            <h2>Swyle is brought to you by the following sponsors</h2>
+            <h2>Swyle wa goran no suponsaa no teikyou de okurishimasu</h2>
             <div className="ad">
-                <h1 className="ad-title">Your Ad Here</h1>
+                <h3 className="ad-title">Your Ad Here</h3>
                 <p className="ad-body">Advertise on Swyle!</p>
             </div>
         </div>
