3239690d4a0191828c9bd5aebd36f4b9a1ae9277
genxhero <genxhero@users.noreply.github.com>
2019-09-18 11:45:14 -0700

    create advert component

---
+++ client/src/adverts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Adverts = () => {
+    return (
+        <div className="adverts-page">
+            <div className="ad">
+                <h1 className="ad-title">Your Ad Here</h1>
+                <p className="ad-body">Advertise on Swyle!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Adverts;
