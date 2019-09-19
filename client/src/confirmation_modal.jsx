3c19bb9588c6ea55e0a8e11417d01fe3b6520e90
genxhero <genxhero@users.noreply.github.com>
2019-09-19 10:39:04 -0700

    style confirmation modal

--- client/src/confirmation_modal.jsx
+++ client/src/confirmation_modal.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 const ConfirmationModal = (props) => {
     return (
         <div className="confirmation-modal">
-            <div className="confirmation-form">
+            <div className="confirmation-dialog">
                 <h3>Are you sure you want to delete {props.title}?</h3>
                 <button onClick={props.confirm}>OK</button>
                 <button onClick={props.cancel}>Cancel</button>
