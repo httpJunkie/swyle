c34b87d9e08ecbd7f1dd1d283a2ab9f41910acd5
genxhero <genxhero@users.noreply.github.com>
2019-09-23 11:57:51 -0700

    refactor image post creation to have error handling, add subscription trigger

--- app/graphql/mutations/create_image_post.rb
+++ app/graphql/mutations/create_image_post.rb
@@ -10,12 +10,23 @@
     
 
     def resolve(title: nil, description: "", image: nil)
-        ImagePost.create!(
-            title: title,
-            description: description,
-            user: context[:current_user],
-            image_url: image
-        )
+        # ImagePost.create!(
+        #     title: title,
+        #     description: description,
+        #     user: context[:current_user],
+        #     image_url: image
+        # )
+        image_post = ImagePost.new
+        image_post.title = title
+        image_post.description = description
+        image_post.user = context[:current_user]
+        image_post.image_url = image 
+        if image_post.save
+          SwyleSchema.subscriptions.trigger("imageAdded", {}, image_post)
+          image_post
+        else
+          {errors: image_post.errors.full_messages}
+        end
     end
   end
 end
