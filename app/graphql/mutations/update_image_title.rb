9dbf496f9ca6990c78f1bc5e4c7f3337cb7c74ab
genxhero <genxhero@users.noreply.github.com>
2019-09-23 22:24:45 -0700

    add sub trigger upon image title update

--- app/graphql/mutations/update_image_title.rb
+++ app/graphql/mutations/update_image_title.rb
@@ -9,8 +9,10 @@ module Mutations
           image = ImagePost.find(id)
           return unless image
           image.title = title
-          image.save
-          image
+          if image.save
+            SwyleSchema.subscriptions.trigger("imageUpdated", {}, image)
+            image
+          end
       end
     end
   end
