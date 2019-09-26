c1c31de29ca6c782e360486dc943c447c215cb12
genxhero <genxhero@users.noreply.github.com>
2019-09-26 11:49:49 -0700

    add basic active / inactive logic

--- client/src/trending.jsx
+++ client/src/trending.jsx
@@ -5,14 +5,29 @@ class Trending extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            shutUpLint: true
+            shutUpLint: true,
+            activePane: "likes"
         }
+        this.selectPane = this.selectPane.bind(this)
+    }
+
+    selectPane(e) {
+        e.preventDefault();
+        this.setState({activePane: e.target.name})
     }
 
     render() {
         return (  
         <div className="trending-posts-page">
             <h1 className="trending-posts-title">Most Popular Posts</h1>
+            <div className="trending-posts-nav">
+                <div className={`trending-posts-tab${this.state.activePane === 'likes' ? '-active' : '-inactive'}`} name="likes">
+                    By Likes
+                </div>
+                    <div className={`trending-posts-tab${this.state.activePane === 'comments' ? '-active' : '-inactive'}`} name="comments">
+                    By Comments
+                </div>
+            </div>
             This particular component will use a graphql query finding articles sorted by most likes or whatever.
         </div>
         )
