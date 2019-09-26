8919e2baf1ffb5c4d18b1ef53db17ab95b5f0e5e
genxhero <genxhero@users.noreply.github.com>
2019-09-26 12:13:25 -0700

    Add posts by popularity client side query

---
+++ client/src/queries/posts_by_popularity.js
@@ -0,0 +1,20 @@
+import gql from 'graphql-tag';
+
+const postsByPopularity = gql`
+
+    postsByPopularity {
+    __typename
+    ... on Article {
+      id
+      title
+      snippet
+    }
+    ... on Image {
+      id
+      title
+      image	
+    }
+}
+`
+
+export default postsByPopularity;
