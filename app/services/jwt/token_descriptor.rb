120ebbec672839ad2498c9d5048706f26dffdb85
genxhero <genxhero@users.noreply.github.com>
2019-09-27 16:59:36 -0700

    fix problem maybe

--- app/services/jwt/token_descriptor.rb
+++ app/services/jwt/token_descriptor.rb
@@ -8,7 +8,7 @@ module Jwt::TokenDecryptor
   private
   def decrypt(token)
     begin
-      Jwt.decode(token, Rails.application.secrets.secret_key_base)
+      JWT.decode(token, Rails.application.secrets.secret_key_base)
     rescue 
        raise InvalidTokenError
     end
