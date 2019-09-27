70217a8475545858b746d838e89a3955c0224d9f
genxhero <genxhero@users.noreply.github.com>
2019-09-27 15:10:25 -0700

    refactor logout resolver

--- app/graphql/mutations/logout.rb
+++ app/graphql/mutations/logout.rb
@@ -1,8 +1,12 @@
 module Mutations
     class Logout < BaseMutation    
         field :email, String, null: true
-        def resolve 
-        {email: ""}
+        # def resolve 
+        # {email: ""}
+        # end
+
+        def resolve
+            cookies.delete(:jwt)
         end
     end
   end
