7592bd9e813dd5ba940f995c1cb2ccc7fa1b6cc2
genxhero <genxhero@users.noreply.github.com>
2019-09-27 15:06:21 -0700

    add authenticate_user to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::API
    include ::ActionController::Cookies
+
+
+  def authenticate_user
+    jwt = cookies.signed[:jwt]
+    decode_jwt(jwt)
+  end
+
   private
 
 # def current_user
