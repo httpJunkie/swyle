e698c7ae78251458d53a3665f66ca0fee156ce39
genxhero <genxhero@users.noreply.github.com>
2019-09-28 14:51:09 -0700

    add rails session login stuff to mutation

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -8,24 +8,34 @@ module Mutations
       field :user, Types::UserType, null: true
   
       # def resolve(email: nil)
-       
       #   return unless email
-  
       #   user = User.find_by email: email[:email]
-         
-      #   return unless user
-        
+      #   return unless user  
       #   return unless user.authenticate(email[:password])
-     
       #     token = Base64.encode64(user.email)
       #   { user: user, token: token }
-       
       # end
+
+      # def resolve(email: nil)
+      #   return unless email
+      #   user = User.find_by email: email[:email]
+      #   if user && user.authenticate(email[:password])
+      #     token = Jwt::TokenProvider.(user_id: user.id)
+      #     { user: user, token: token }
+      #   else
+      #    { errors: user.errors.full_messages}
+      #   end
+      # end
+
+      
       def resolve(email: nil)
         return unless email
         user = User.find_by email: email[:email]
         if user && user.authenticate(email[:password])
-          token = Jwt::TokenProvider.(user_id: user.id)
+          user.reset_token
+          context[:current_user] = user 
+          token = "poop"
+          debugger
           { user: user, token: token }
         else
          { errors: user.errors.full_messages}
