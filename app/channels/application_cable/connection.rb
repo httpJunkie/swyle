f47a841176359ab66559e2c59105e772fb76e3ad
genxhero <genxhero@users.noreply.github.com>
2019-09-28 13:55:10 -0700

    update connect to use jwt

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,10 +8,17 @@ module ApplicationCable
 
     private
 
-    def current_user
-      token = request.params[:token].to_s
-      email = Base64.decode64(token)
-      User.find_by(email: email)
+    # def current_user
+    #   token = request.params[:token].to_s
+    #   email = Base64.decode64(token)
+      
+    #   User.find_by(email: email)
+    # end
+
+    def current_user 
+      user ||= Jwt::UserAuthenticator.(request.headers)
+      user
     end
+    
   end
 end
