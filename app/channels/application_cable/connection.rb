5015aef0da1e2510eb97b988be2d90205f4c3e3d
genxhero <genxhero@users.noreply.github.com>
2019-09-29 05:37:35 -0700

    clean up connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,15 +8,6 @@ module ApplicationCable
 
     private
 
-    # def current_user
-    #   # user ||= Jwt::UserAuthenticator.(request.headers)
-    #   butt = "big"
-    #   debugger
-    #   user ||= User.find(session_token: session[:session_token])
-
-    #   user
-    # end
-
     def current_user       
       user = User.find_by(id: cookies.signed[:user_id])
       user 
