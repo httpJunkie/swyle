6752c628abaec7808b4043e83c603daafaaf4c3d
genxhero <genxhero@users.noreply.github.com>
2019-09-29 05:29:37 -0700

    remove deprecated resolver

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -8,17 +8,6 @@ module Mutations
       field :token, String, null: true
       field :user, Types::UserType, null: true
 
-      # def resolve(email: nil)
-      #   return unless email
-      #   user = User.find_by email: email[:email]
-      #   if user && user.authenticate(email[:password])
-      #     token = Jwt::TokenProvider.(user_id: user.id)
-      #     { user: user, token: token }
-      #   else
-      #    { errors: user.errors.full_messages}
-      #   end
-      # end
-
       def resolve(email: nil)
         return unless email
         user = User.find_by email: email[:email]
