92326929b7342e8c885b5df44a2a1ebc83182416
genxhero <genxhero@users.noreply.github.com>
2019-09-29 05:23:48 -0700

    delete useless current user code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 class ApplicationController < ActionController::API
     helper_method :current_user
     helper_method :cookies
-
-    # def current_user
-    #     token = request.headers["Authorization"].to_s
-    #     email = Base64.decode64(token)
-    #     User.find_by(email: email)
-    # end    
-
-
 end
