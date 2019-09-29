9a01e8640b40645eceeec31abb7ede4432106e18
genxhero <genxhero@users.noreply.github.com>
2019-09-29 05:34:55 -0700

    remove useless method

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,13 +9,6 @@ module ApplicationCable
     private
 
     # def current_user
-    #   token = request.params[:token].to_s
-    #   email = Base64.decode64(token)
-      
-    #   User.find_by(email: email)
-    # end
-
-    # def current_user
     #   # user ||= Jwt::UserAuthenticator.(request.headers)
     #   butt = "big"
     #   debugger
