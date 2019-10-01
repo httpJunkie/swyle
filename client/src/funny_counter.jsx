b4b3920e62d844c9c06e42ff89bdcb97a67adb37
genxhero <genxhero@users.noreply.github.com>
2019-10-01 11:38:07 -0700

    funny section part 1

---
+++ client/src/funny_counter.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { MdThumbUp, MdThumbDown } from 'react-icons/md';
+import { Mutation } from 'react-apollo';
+import createFunny from './mutations/create_funny';
+import article from './queries/article';
+import image from './queries/image';
+import deleteFunny from './mutations/delete_funny';
+
+const QUERIES = { "Article": article, "ImagePost": image };
+
+class FunnyCounter extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: this.props.currentUser
+        }
+    }
+
+
+    componentWillReceiveProps(newProps) {
+        if (this.props.currentUser !== newProps.currentUser) {
+            this.setState({ currentUser: newProps.currentUser })
+        }
+    }
+
+    render() {
+        const userAlreadyLaughed = this.state.currentUser && this.props.laughers.includes(this.state.currentUser.id);
+        const refetch = QUERIES[this.props.type]
+        return (
+            <div className="likes-section" style={{ "color": "white" }}>
+                <div className="like-or-dislike">
+                    {userAlreadyLaughed ?
+                        <Mutation
+                            mutation={unlikePost}
+                            refetchQueries={[{ query: refetch, variables: { id: this.props.postId, } }]}
+                            update={(cache, { data: { unlikePost } }) => {
+                            }}>
+                            {(unlikePost, loading) =>
+                                !loading ? (
+                                    "..."
+                                ) : (
+                                        <span>
+                                            <MdThumbUp className="like-thumb-yes"
+                                                onClick={event => {
+                                                    event.preventDefault();
+                                                    if (!this.state.currentUser) {
+                                                        return false;
+                                                    }
+                                                    unlikePost({
+                                                        variables: {
+                                                            userId: this.state.currentUser.id,
+                                                            postId: this.props.postId,
+                                                            postType: this.props.type
+                                                        }
+                                                    }).then(res => {
+                                                        // this.setState({ body: "" })
+                                                    })
+
+                                                }}
+                                            />
+                                            {this.props.numLikes}
+                                        </span>
+                                    )}
+                        </Mutation>
+                        :
+                        <Mutation
+                            mutation={likePost}
+                            refetchQueries={[{ query: refetch, variables: { id: this.props.postId, } }]}
+                            update={(cache, { data: { likePost } }) => {
+                            }}>
+                            {(likePost, loading) =>
+                                !loading ? (
+                                    "..."
+                                ) : (
+                                        <span>
+                                            <MdThumbUp className="like-thumb-no"
+                                                onClick={event => {
+                                                    event.preventDefault();
+                                                    if (!this.state.currentUser) {
+                                                        alert("Find this funny? Log in to have it persist!")
+                                                        return false;
+                                                    }
+                                                    likePost({
+                                                        variables: {
+                                                            userId: this.state.currentUser.id,
+                                                            postId: this.props.postId,
+                                                            postType: this.props.type
+                                                        }
+                                                    }).then(res => {
+                                                        // this.setState({ body: "" })
+                                                    })
+
+                                                }}
+                                            />
+                                            {this.props.numLikes}
+                                        </span>
+                                    )}
+                        </Mutation>
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default FunnyCounter;
