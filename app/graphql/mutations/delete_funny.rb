86ce03103155da4d4bfaaec1601f105980879d18
genxhero <genxhero@users.noreply.github.com>
2019-09-30 20:34:35 -0700

    add delete funny mutation

---
+++ app/graphql/mutations/delete_funny.rb
@@ -0,0 +1,23 @@
+module Mutations
+    class Deletefunny < BaseMutation    
+        argument :post_type, String, required: true
+        argument :user_id, Integer, required: true
+        argument :post_id, Integer, required: true
+        type Types::FunnyType
+        def resolve(user_id: nil, post_id: nil, post_type: nil)
+            funny = Funny.find_by(user_id: user_id, post_id: post_id, post_type: post_type)
+            funny.destroy
+            if post_type === "Article"
+                article = Article.find(post_id.to_i)
+                # SwyleSchema.subscriptions.trigger("articleUnliked", {}, article)
+                article
+            end
+
+            if post_type === "ImagePost"
+                image = ImagePost.find(post_id.to_i)
+                # SwyleSchema.subscriptions.trigger("imageUnliked", {}, image)
+                image
+            end
+        end
+    end
+  end
