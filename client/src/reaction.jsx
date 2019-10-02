3b43863367a3bcee89224e5e47a52920a1938037
genxhero <genxhero@users.noreply.github.com>
2019-10-01 21:04:53 -0700

    Add document comment to reaction

--- client/src/reaction.jsx
+++ client/src/reaction.jsx
@@ -1,3 +1,11 @@
+/**
+ * Expected Props
+ *     users: array of integers, IDs of users who have reacted
+ *     type: string, type of reaction
+ *     count: integer, number of current reactions of this type
+ *     currentUser: Object representing a user.
+ */
+
 import React, { Component } from 'react';
 import { MdThumbUp } from 'react-icons/md';
 import { Mutation } from 'react-apollo';
@@ -14,7 +22,29 @@ const CREATE_MUTATIONS = {'like': likePost, 'funny': createFunny}
 const DELETE_MUTATIONS = {'like': unlikePost, 'funny': deleteFunny}
 
 class Reaction extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: this.props.currentUser
+        }
+    }
 
+
+    componentDidUpdate(newProps) {
+        if (this.props.currentUser !== newProps.currentUser) {
+            this.setState({ currentUser: newProps.currentUser })
+        }
+    }
+
+    render() {
+        const userReacted = this.state.currentUser && this.props.users.includes(this.state.currentUser.id);
+        const refetch = QUERIES[this.props.type]
+        return (
+            <div>
+
+            </div>
+        )
+    }
 }
 
 export default Reaction
