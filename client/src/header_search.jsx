971c92211a90543dbc9cac880e6463e2ff1c36de
genxhero <genxhero@users.noreply.github.com>
2019-10-26 12:01:50 -0700

    add hooks to search component

--- client/src/header_search.jsx
+++ client/src/header_search.jsx
@@ -1,12 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const HeaderSearch = () => {
+    const handleInputChange = e => {
+        const { name, value } = e.target
+        setValues({ ...values, [name]: value })
+    }
+    
+    const [query, setQuery] = useState("");
+
     return (
         <form className="header-search">
             <input className="header-search-bar" type="text">
             </input>
-            <div className="header-search-submit" />
-
+            <Link className="header-search-submit" to={`/search?q=${query}`}/>
         </form>
     )
 }
