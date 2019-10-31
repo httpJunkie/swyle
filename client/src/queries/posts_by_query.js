628c624213882c8a6232e14c12e6537d32c43671
genxhero <genxhero@users.noreply.github.com>
2019-10-30 23:05:04 -0700

    add working query to client side query folder. you know, for searches

--- client/src/queries/posts_by_query.js
+++ client/src/queries/posts_by_query.js
@@ -1,7 +1,21 @@
 import gql from 'graphql-tag';
 
 const search = gql`
-
+postsByQuery($searchQuery: String!){
+    postsByQuery(searchQuery: $searchQuery) {
+    __typename
+    ... on Article {
+      id
+      title
+      snippet
+    }
+    ... on Image {
+      id
+      title
+      image	
+    }
+  }
+}
 `;
 
 export default search;
