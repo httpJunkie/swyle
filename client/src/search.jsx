834d8ba82421ae444adb5a7dfec3730da10eab9b
genxhero <genxhero@users.noreply.github.com>
2019-10-30 23:21:48 -0700

    add logic for grabbing search query to search

--- client/src/search.jsx
+++ client/src/search.jsx
@@ -3,10 +3,10 @@ import { withApollo, Query } from 'react-apollo'
 import search from './queries/posts_by_query';
 
 const Search = (props) =>  {
-    
+        const query = props.location.search;
         console.log(props.location.search)
        return (           <div>
-           <Query query={search}>
+           <Query query={search} variables={{ searchQuery: query }}>
                 {({ loading, error, data }) => {
                if (loading) return <p>Loading...</p>;
                if (error) return <p>Error :(</p>;
