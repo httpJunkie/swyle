c0673b9be8989233e7c9b212afce53a03cbb3e0d
genxhero <genxhero@users.noreply.github.com>
2019-11-01 09:18:31 -0700

    add helper function for parsin a url

--- client/src/search.jsx
+++ client/src/search.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { withApollo, Query } from 'react-apollo'
 import search from './queries/posts_by_query';
+import {Link} from 'react-router-dom';
 
 const Search = (props) =>  {
         const query = props.location.search;
-        console.log(props.location.search)
+        const parseUrl = (string) => {
+            switch (string) {
+                case "Image":
+                    return '/images';
+                case "Article":
+                    return '/articles';
+            }
+        }
        return (           <div>
            <Query query={search} variables={{ searchQuery: query }}>
                 {({ loading, error, data }) => {
@@ -16,9 +24,10 @@ const Search = (props) =>  {
                return (
                    <div>
                        {data.postsByQuery.map(post => {
+                           const url = parseUrl(post.__typename)
                            return (
                                <div className="user-posts-card" key={`${post.title}${post.title.length}${post.id}`}>
-                                   <h3 >{post.title}</h3>
+                                   <h3 ><Link>{post.title}</Link></h3>
                                    {post.image && <div className="user-posts-thumbnail-container"><img src={post.image} /> </div>}
                                    {post.snippet && <p>{post.snippet}<span style={{ "color": "gray" }}>...</span></p>}
                                </div>
