51dd95ee515553ef2178ea631b99bea12f07982e
genxhero <genxhero@users.noreply.github.com>
2019-11-02 11:16:06 -0700

    change prev.articles to prev.postsbyquery

--- client/src/search_subscription.jsx
+++ client/src/search_subscription.jsx
@@ -8,19 +8,25 @@ const Subscription = ({ subscribeToMore }) => {
             document: SearchSubscription,
             updateQuery: (prev, { subscriptionData }) => {
                 if (!subscriptionData.data) return prev;
-
+                debugger;
                 const { imageAdded } = subscriptionData.data;
 
                 if (imageAdded) {
-                    return {
-                        image: imageAdded
+                    const alreadyInList = prev.postsByQuery.find(e => e.id === imageAdded.id && e.__typename === "Image");
+                    if (alreadyInList) {
+                        return prev;
                     }
+                    return { postsByQuery: [imageAdded].concat(prev.postsByQuery) };
                 }
+
                 const { articleAdded } = subscriptionData.data
                 if (articleAdded) {
-                    return {
-                        article: articleAdded
+                    const alreadyInList = prev.postsByQuery.find(e => e.id === articleAdded.id && e.__typename === "Article");
+              
+                    if (alreadyInList) {
+                        return prev;
                     }
+                    return { postsByQuery: [articleAdded].concat(prev.postsByQuery) };
                 }
                 return prev;
             },
