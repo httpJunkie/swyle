16d65a6183268662f8cb24fe852b0c8c631f1c13
genxhero <genxhero@users.noreply.github.com>
2019-11-02 10:47:50 -0700

    try experimental search subscription

--- client/src/subscriptions/search_subscription.js
+++ client/src/subscriptions/search_subscription.js
@@ -1,6 +1,18 @@
 import gql from 'graphql-tag';
 
 const SearchSubscription = gql`
+subscription SearchSubscription {
+    imageAdded {
+        id
+        title
+        image
+   },
+    articleAdded {
+        id
+        title
+        snippet
+   },
+}
 `
 
 export default SearchSubscription;
