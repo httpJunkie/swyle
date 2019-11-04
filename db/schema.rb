fe2b934278d970185ceb1e7620db62853d8fe385
genxhero <genxhero@users.noreply.github.com>
2019-11-03 18:16:20 -0800

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_201408) do
+ActiveRecord::Schema.define(version: 2019_11_04_021314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,16 @@ ActiveRecord::Schema.define(version: 2019_10_01_201408) do
     t.string "post_type"
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "spicies", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.string "post_type", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_spicies_on_post_id"
+    t.index ["user_id"], name: "index_spicies_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
