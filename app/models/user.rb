98611e8a946ff49108a7631d9c82863a82ce4a8a
genxhero <genxhero@users.noreply.github.com>
2019-11-06 08:48:04 -0800

    comments for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+#Exactly what it says on the tin
 class User < ApplicationRecord
  
   has_secure_password
@@ -29,13 +30,13 @@ class User < ApplicationRecord
   foreign_key: :user_id,
   class_name: "Funny"
 
-
+#Resets the session token, returns the session token.
   def reset_token
     self.session_token = Jwt::TokenProvider.(user_id: self.id)
     self.save!
     self.session_token
   end
-
+#Returns a new session token...shouldnt this be ||=? TODO: Check.
   def ensure_token
     self.session_token = Jwt::TokenProvider.(user_id: self.id)
   end
