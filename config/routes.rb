dbd048b6d99199352b9c937b0c93c7099ccfe30c
genxhero <genxhero@users.noreply.github.com>
2019-11-07 09:55:51 -0800

    activate version 2 of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
-# Rails.application.routes.draw do
-#   post "/graphql", to: "graphql#execute"
-#   if Rails.env.development? 
-#     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-#     root to: redirect("/graphiql")
-#   end
-#   mount ActionCable.server, at: '/cable'
-#   # root :to =>  'index#index'
-#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# end
-
 Rails.application.routes.draw do
-  if Rails.env.development? || Rails.env.staging? || Rails.env.production?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
   post "/graphql", to: "graphql#execute"
+  if Rails.env.development? 
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    root to: redirect("/graphiql")
+  end
   mount ActionCable.server, at: '/cable'
-  root :to =>  'index#index'
-  # root to: redirect("/graphiql")
+  # root :to =>  'index#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# Rails.application.routes.draw do
+#   if Rails.env.development? || Rails.env.staging? || Rails.env.production?
+#     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+#   end
+#   post "/graphql", to: "graphql#execute"
+#   mount ActionCable.server, at: '/cable'
+#   root :to =>  'index#index'
+#   # root to: redirect("/graphiql")
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
