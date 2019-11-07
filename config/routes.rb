e4ec8e7ed658e6bbcbbc25d9b3cb821a4eba035e
genxhero <genxhero@users.noreply.github.com>
2019-11-07 09:42:46 -0800

    fuss about with routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  if Rails.env.development? || Rails.env.staging? || Rails.env.production?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
   post "/graphql", to: "graphql#execute"
+  if Rails.env.development? 
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    root to: redirect("/graphiql")
+  end
   mount ActionCable.server, at: '/cable'
-  root :to =>  'index#index'
-  # root to: redirect("/graphiql")
+  # root :to =>  'index#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
