ae1c6ee8b5166ccc8837f1ff49145e2460df5039
genxhero <genxhero@users.noreply.github.com>
2019-11-08 09:00:45 -0800

    documentation for confirmation modal

--- client/src/confirmation_modal.jsx
+++ client/src/confirmation_modal.jsx
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
 import {MdCheck, MdClose} from 'react-icons/md';
 
+/**
+ * Modal which displays upon an attempt to delete an article or image post.
+ * 
+ * Expected Props
+ *      title: String - the name of the thing to be deleted
+ *      confirm: Function - should make a DELETE request to one's backend; or it could even be used 
+ *                          to just remove something from the DOM which only exists in state.
+ *      cancel: Function - should close the modal using setState or the useState hook.
+ */
+
 const ConfirmationModal = (props) => {
     return (
         <div className="confirmation-modal">
