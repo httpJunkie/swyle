79faa23237744dabd7d959314956e3d1a4ed6381
genxhero <genxhero@users.noreply.github.com>
2019-11-08 08:43:35 -0800

    add comment for article tag section

--- client/src/article_tags.jsx
+++ client/src/article_tags.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/**
+ * 
+ * This displays all of the tags belonging to an article.
+ * TODO: Change to PostTags
+ */
 const ArticleTags = (props) => {
     return (
         <span className="article-card-tags">{props.tags.map( tag => <span key={tag}>{`#${tag} `}</span>)}</span>
