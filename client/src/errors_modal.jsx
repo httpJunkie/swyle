91ad07922626b1293ef5e8f2bf08f7a36137640b
genxhero <genxhero@users.noreply.github.com>
2019-11-08 09:05:56 -0800

    asS

--- client/src/errors_modal.jsx
+++ client/src/errors_modal.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+/**
+ * 
+ * Expected Props
+ *      errors: An array of error objects containing a "message" key at the very minimum
+ *      clearErrors: A function bound to the parent component which removes errors and changes the state to no longer
+ *                   be displaying the modal.  Eg, setErrors(null), this.setState({errors: null})
+ */
+
 const ErrorsModal = (props) => {
     return (
         <div className="auth-error-modal">
