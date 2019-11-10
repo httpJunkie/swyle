26e46019200a86c342530fbdc84facf6a2def528
genxhero <genxhero@users.noreply.github.com>
2019-11-10 08:22:13 -0800

    comment for new like mutation

--- client/src/mutations/like_post.js
+++ client/src/mutations/like_post.js
@@ -1,3 +1,11 @@
+/**
+ * Mutation to be called when a user clicks the thumbs up icon in the Reactions tab.
+ * Expected Arguments
+ *      postType: string, determines which table the post belongs to (in this case articles or image_posts).
+ *      postId:  integer, it's an ID number for a post (Article or ImagePost). Will be come a foreign key on the new Like
+ *      userId:  integer, it's an ID number for a User.
+ */
+
 import gql from 'graphql-tag'
 
 const likePost = gql`
