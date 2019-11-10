77897932cb99ae3a010e8c60303daccfb197ed9b
genxhero <genxhero@users.noreply.github.com>
2019-11-10 08:13:20 -0800

    comment for current user

--- client/src/queries/current_user.js
+++ client/src/queries/current_user.js
@@ -1,3 +1,7 @@
+/**
+ * Fetches the user currently logged in, can be reused in just about any back end as long as the fields happen to be the same.
+ */
+
 import gql from 'graphql-tag';
 
 const currentUser = gql`{
