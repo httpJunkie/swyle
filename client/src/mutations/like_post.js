39eec2e2e4569c0918f1164401ef8c15f908cd50
genxhero <genxhero@users.noreply.github.com>
2019-11-10 08:25:00 -0800

    create a todo documentation for reaction mutations

--- client/src/mutations/like_post.js
+++ client/src/mutations/like_post.js
@@ -4,6 +4,10 @@
  *      postType: string, determines which table the post belongs to (in this case articles or image_posts).
  *      postId:  integer, it's an ID number for a post (Article or ImagePost). Will be come a foreign key on the new Like
  *      userId:  integer, it's an ID number for a User.
+ * 
+ * TODO: Refactor to more generic 'reaction', add a new argument "reactionType" actually it needs a better name than that since
+ *       reactionType can also be a GraphQL typing name. "typeOfReaction"? Need to make a new Union as well and then i can get all the reactions
+ *       with a single snippet.
  */
 
 import gql from 'graphql-tag'
