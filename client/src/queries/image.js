f0849c1e4404ccb8934cca53b65385b8cff1ee32
genxhero <genxhero@users.noreply.github.com>
2019-11-10 08:14:41 -0800

    comment for image query

--- client/src/queries/image.js
+++ client/src/queries/image.js
@@ -1,3 +1,8 @@
+/**
+ * Fetches a single image post, accepts an integer argument that must be a valid ID.
+ * TODO: Make fragments and split this sucker up.
+ */
+
 import gql from "graphql-tag";
 
 const image = gql`
