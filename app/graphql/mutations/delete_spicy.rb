b44af5df5db38b0fc300f82d56d2ea1895468251
genxhero <genxhero@users.noreply.github.com>
2019-11-11 08:38:34 -0800

    create delete spicy

---
+++ app/graphql/mutations/delete_spicy.rb
@@ -0,0 +1,23 @@
+module Mutations
+    class DeleteSpicy < BaseMutation    
+        argument :post_type, String, required: true
+        argument :user_id, Integer, required: true
+        argument :post_id, Integer, required: true
+        type Types::SpicyType
+        def resolve(user_id: nil, post_id: nil, post_type: nil)
+            spicy = Spicy.find_by(user_id: user_id, post_id: post_id, post_type: post_type)
+            spicy.destroy
+            if post_type === "Article"
+                article = Article.find(post_id.to_i)
+                SwyleSchema.subscriptions.trigger("articleUnliked", {}, article)
+                article
+            end
+
+            if post_type === "ImagePost"
+                image = ImagePost.find(post_id.to_i)
+                SwyleSchema.subscriptions.trigger("imageUnliked", {}, image)
+                image
+            end
+        end
+    end
+  end
