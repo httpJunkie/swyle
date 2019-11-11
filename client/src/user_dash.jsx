08d64bbdfc7ac6fbb49f6be3aa91894873e39737
genxhero <genxhero@users.noreply.github.com>
2019-11-11 09:35:33 -0800

    fix potential error

--- client/src/user_dash.jsx
+++ client/src/user_dash.jsx
@@ -53,7 +53,7 @@ class UserDash extends Component {
                                           return(
                                             <div className="user-posts-card" key={`${post.title}${post.id}`}>
                                                 <h3 >{post.title}</h3>
-                                                {post.image && <div className="user-posts-thumbnail-container"><img src={post.image} /> </div>}
+                                                {post.image && <div className="user-posts-thumbnail-container"><img src={post.image} alt={post.image.title} /> </div>}
                                                 {post.snippet && <p>{post.snippet}<span style={{"color":"gray"}}>...</span></p>}
                                             </div>
                                             )
