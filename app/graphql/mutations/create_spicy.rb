e89bab8fce465ef33c9285532fb922306785d847
genxhero <genxhero@users.noreply.github.com>
2019-11-11 08:31:24 -0800

    add spicy creation mutation

---
+++ app/graphql/mutations/create_spicy.rb
@@ -0,0 +1,24 @@
+module Mutations
+    class CreateSpicy < BaseMutation    
+        argument :post_type, String, required: true
+        argument :user_id, Integer, required: true
+        argument :post_id, Integer, required: true
+        type Types::SpicyType
+        def resolve(user_id: nil, post_id: nil, post_type: nil)
+            spicy = Spicy.new
+            spicy.user_id = user_id
+            spicy.post_id = post_id
+            spicy.post_type = post_type
+            spicy.save
+            if post_type === "Article"
+                article = Article.find(post_id.to_i)
+                # SwyleSchema.subscriptions.trigger("articleLiked", {}, article)
+            end
+            if post_type === "ImagePost"
+                image = ImagePost.find(post_id.to_i)
+                # SwyleSchema.subscriptions.trigger("imageLiked", {}, image)
+            end
+            spicy
+        end
+    end
+  end
