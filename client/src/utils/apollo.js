112e988828dd25ef98c4e2ed7aad583983d86995
genxhero <genxhero@users.noreply.github.com>
2019-11-11 21:40:29 -0800

    see what happens with the protocol

--- client/src/utils/apollo.js
+++ client/src/utils/apollo.js
@@ -13,10 +13,15 @@ import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
  */
 
 const getCableUrl =  () => {
-    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    // const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const protocol = 'ws';
     const host = window.location.hostname;
     const port = process.env.CABLE_PORT || '3000';
     const authToken =  localStorage.getItem('mlToken');
+    console.log(
+      "Token Link thingy",
+      `${protocol}//${host}:${port}/cable?token=${authToken}`
+    );
     return `${protocol}//${host}:${port}/cable?token=${authToken}`;
 };
 
