d2ca15cc9918d8716365270ad592b021b74eb98b
genxhero <genxhero@users.noreply.github.com>
2019-11-13 08:13:18 -0800

    error message styling

--- client/src/errors_modal.jsx
+++ client/src/errors_modal.jsx
@@ -10,9 +10,9 @@ import React from 'react';
 
 const ErrorsModal = (props) => {
     return (
-        <div className="auth-error-modal">
-            <div className="auth-error-message">
-                {props.errors.map(error => <span key={error.message + "isthekey"}>{error.message}</span>)}
+        <div className="error-modal">
+            <div className="error-dialog">
+                {props.errors.map(error => <span className="error-message" key={error.message + "isthekey"}>{error.message}</span>)}
                 <button onClick={props.clearErrors}>OK</button>
             </div>
         </div>
