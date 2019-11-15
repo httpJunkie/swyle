d68dd694360346f4243119c51f3e53019eb28641
genxhero <genxhero@users.noreply.github.com>
2019-11-15 11:41:22 -0800

    put correct subscription into backend

--- app/graphql/mutations/delete_spicy.rb
+++ app/graphql/mutations/delete_spicy.rb
@@ -9,13 +9,13 @@ module Mutations
             spicy.destroy
             if post_type === "Article"
                 article = Article.find(post_id.to_i)
-                SwyleSchema.subscriptions.trigger("articleUnliked", {}, article)
+                SwyleSchema.subscriptions.trigger("articleUpdated", {}, article)
                 article
             end
 
             if post_type === "ImagePost"
                 image = ImagePost.find(post_id.to_i)
-                SwyleSchema.subscriptions.trigger("imageUnliked", {}, image)
+                SwyleSchema.subscriptions.trigger("imageUpdated", {}, image)
                 image
             end
         end
