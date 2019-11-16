aa7a0156b4898eadb95b14c0935b4b41fefb7d19
genxhero <genxhero@users.noreply.github.com>
2019-11-16 11:07:54 -0800

    change article tags to just tags

--- client/src/article_tags.jsx
+++ client/src/article_tags.jsx
@@ -5,10 +5,10 @@ import React from 'react';
  * This displays all of the tags belonging to an article.
  * TODO: Change to PostTags
  */
-const ArticleTags = (props) => {
+const Tags = (props) => {
     return (
         <span className="article-card-tags">{props.tags.map( tag => <span key={tag}>{`#${tag} `}</span>)}</span>
     )
 }
 
-export default ArticleTags;
+export default Tags;
