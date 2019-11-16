18f15c7959878bf2c4809d5aea0711495475725a
genxhero <genxhero@users.noreply.github.com>
2019-11-16 10:59:07 -0800

    remove another function that was commentecd out

--- client/src/utils/apollo.js
+++ client/src/utils/apollo.js
@@ -138,15 +138,3 @@ export const createCache = () => {
     }
     return cache;
 };
-
-
-// export const createClient = (cache, requestLink) => {
-//     return new ApolloClient({
-//         link: ApolloLink.from([
-//             createErrorLink(),
-//             createLinkWithToken(),
-//             createHttpLink(),
-//         ]),
-//         cache,
-//     });
-// };
