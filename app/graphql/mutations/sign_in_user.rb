59287087b744692fceabc77f6234b28ff753dac8
genxhero <genxhero@users.noreply.github.com>
2019-11-17 11:24:53 -0800

    more logging

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -12,6 +12,7 @@ module Mutations
         return unless email
         user = User.find_by email: email[:email]
         if user && user.authenticate(email[:password])
+          puts "Authentication SUCCESS FOR #{user.username}"
           context[:session][:session_token] = user.reset_token        
           context[:current_user] = user 
           context[:cookies].signed[:user_id] = user.id
