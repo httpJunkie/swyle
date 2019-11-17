f36a9595c5dbfb0ad947939b14bab1eeed3699ee
genxhero <genxhero@users.noreply.github.com>
2019-11-17 11:26:23 -0800

    sorry git, didnt mean to yoyo you

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -13,11 +13,11 @@ module Mutations
         user = User.find_by email: email[:email]
         if user && user.authenticate(email[:password])
           puts "Authentication SUCCESS FOR #{user.username}"
-          puts "Before Reset: #{context[:session][:session_token] }"
+          puts "Before Reset: #{context[:session][:session_token] } User's: #{user.session_token}"
           context[:session][:session_token] = user.reset_token        
           context[:current_user] = user 
           context[:cookies].signed[:user_id] = user.id
-          puts "After Reset: #{context[:session][:session_token] }"
+          puts "After Reset: #{context[:session][:session_token]} User's: #{user.session_token}"
           # This token is used exclusively for websockets
           token = SecureRandom::urlsafe_base64
           { user: user, token: token }
