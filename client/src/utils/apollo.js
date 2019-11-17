4802c418b63f797fe0de0a3d955b33dd211bacf0
genxhero <genxhero@users.noreply.github.com>
2019-11-17 10:28:49 -0800

    add default string to apollo

--- client/src/utils/apollo.js
+++ client/src/utils/apollo.js
@@ -17,7 +17,7 @@ const getCableUrl =  () => {
     // const protocol = 'wss:';
     const host = window.location.hostname;
     const port = process.env.CABLE_PORT || '3000';
-    const authToken =  localStorage.getItem('mlToken');
+    const authToken =  localStorage.getItem('mlToken') || 'loggedouttoken';
     console.log("Cable Token:", authToken);
     // return `${protocol}//${host}:${port}/cable?token=${authToken}`;
     return `wss://afternoon-eyrie-69554.herokuapp.com/cable?token=${authToken}`;
