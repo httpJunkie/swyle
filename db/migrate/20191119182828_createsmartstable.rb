0085aaf84310b9aed84e6e874bfe9ec403847c90
genxhero <genxhero@users.noreply.github.com>
2019-11-19 10:29:51 -0800

    write up the migration

--- db/migrate/20191119182828_createsmartstable.rb
+++ db/migrate/20191119182828_createsmartstable.rb
@@ -1,4 +1,12 @@
 class Createsmartstable < ActiveRecord::Migration[6.0]
-  def change
+      create_table :smarts do |t|
+      t.integer :user_id, null: false
+      t.integer :post_id, null: false
+      t.string :post_type, null: false
+      t.timestamps
+
+    end
+    add_index :smarts, :user_id
+    add_index :smarts, :post_id
   end
 end
