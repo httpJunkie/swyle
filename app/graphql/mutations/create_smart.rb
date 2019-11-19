d5de0bc21e6910f47e8ffa04dcd9f5face055ed5
genxhero <genxhero@users.noreply.github.com>
2019-11-19 10:44:28 -0800

    backend mutation for create smart

---
+++ app/graphql/mutations/create_smart.rb
@@ -0,0 +1,24 @@
+module Mutations
+    class CreateSmart < BaseMutation    
+        argument :post_type, String, required: true
+        argument :user_id, Integer, required: true
+        argument :post_id, Integer, required: true
+        type Types::SmartType
+        def resolve(user_id: nil, post_id: nil, post_type: nil)
+            smart = smart.new
+            smart.user_id = user_id
+            smart.post_id = post_id
+            smart.post_type = post_type
+            smart.save
+            if post_type === "Article"
+                article = Article.find(post_id.to_i)
+                SwyleSchema.subscriptions.trigger("articleUpdated", {}, article)
+            end
+            if post_type === "ImagePost"
+                image = ImagePost.find(post_id.to_i)
+                SwyleSchema.subscriptions.trigger("imageUpdated", {}, image)
+            end
+            smart
+        end
+    end
+  end
