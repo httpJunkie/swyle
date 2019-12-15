c5ce36324e52bec0a44c48e885b27bc54fec8960
genxhero <genxhero@users.noreply.github.com>
2019-12-15 10:55:16 -0800

    remove loggers

--- app/graphql/mutations/logout.rb
+++ app/graphql/mutations/logout.rb
@@ -4,13 +4,11 @@ module Mutations
 
         def resolve
             user = context[:current_user]
-            puts "Logging Out: #{context[:session][:session_token]}";
             user.reset_token
             context[:current_user] = nil
             context[:session][:session_token] = nil
             #Maybe this?
             context[:cookies].signed[:user_id] = nil
-            puts "After The Logout: #{context[:session][:session_token]}";
             token = SecureRandom::urlsafe_base64
             {email: token}
         end
