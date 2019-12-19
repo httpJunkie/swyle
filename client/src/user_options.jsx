9a29db29f6fdd1d159a16b3e204acd303b768e67
genxhero <genxhero@users.noreply.github.com>
2019-12-19 10:40:49 -0800

    set up use state hook for color scheme

--- client/src/user_options.jsx
+++ client/src/user_options.jsx
@@ -3,8 +3,12 @@ import mutation from './mutations/update_user_color_scheme';
 import graphql from 'react-apollo';
 
 const UserOptions  = (props) => {
+    const [colorScheme, changeColorScheme] = useState(props.colorScheme)
     return (
         <div className="user-options">
+            <form>
+
+            </form>
 
         </div>
     )
