bd1195023e1bad2aeef5da62794d5964303009ac
genxhero <genxhero@users.noreply.github.com>
2019-12-19 10:30:31 -0800

    import the mutation

--- client/src/user_options.jsx
+++ client/src/user_options.jsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
+import colorScheme from './mutations/update_user_color_scheme';
 
 const UserOptions  = (props) => {
     return (
-        <div>
+        <div className="user-options">
 
         </div>
     )
