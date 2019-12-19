4e66bbe93f833290c43f58c985792eb1faa203ed
genxhero <genxhero@users.noreply.github.com>
2019-12-19 10:54:08 -0800

    add onclick to the spans

--- client/src/user_options.jsx
+++ client/src/user_options.jsx
@@ -4,13 +4,20 @@ import {graphql} from 'react-apollo';
 
 const UserOptions  = (props) => {
     const [colorScheme, changeColorScheme] = useState(props.colorScheme)
+    const handleClick = (event) => {
+        debugger;
+        changeColorScheme(event.target.name)
+       this.props.mutate({}).then( res => {
+            debugger;
+        })
+    }
     return (
         <div className="user-options">
             <div className="user-option">
                 <h4>Color Scheme</h4>
-                <span className={colorScheme === "standard" ? "option-selected" : "option-unselected"}>Classic</span>
-                <span className={colorScheme === "red" ? "option-selected" : "option-unselected"}>Radical Red</span>
-                <span className={colorScheme === "green" ? "option-selected" : "option-unselected"}>Groovy Green</span>
+                <span onClick={handleClick} name="standard" className={colorScheme === "standard" ? "option-selected" : "option-unselected"}>Classic</span>
+                <span onClick={handleClick} name="red" className={colorScheme === "red" ? "option-selected" : "option-unselected"}>Radical Red</span>
+                <span onClick={handleClick} name="green" className={colorScheme === "green" ? "option-selected" : "option-unselected"}>Groovy Green</span>
             </div>
 
         </div>
