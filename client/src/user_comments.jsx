0e40eb7317467d7f508cacb6534489caf4077503
genxhero <genxhero@users.noreply.github.com>
2019-12-21 14:15:43 -0800

    user comments to own component

--- client/src/user_comments.jsx
+++ client/src/user_comments.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
 const UserComments = (props) => {
-
+    return (
+        <div className="user-comments">
+            <h3 style={{ "textAlign": "center", "color": "white" }}> Comments</h3>
+            <Query query={commentsByUser} variables={{ userId: props.userId }}>
+                {({ loading, error, data }) => {
+                    if (loading) return <p>Loading...</p>;
+                    if (error) return <p>Error </p>;
+                    return (
+                        <div>
+                            {data.commentsByUser.map(comment => {
+                                return (
+                                    <div className="user-comments-card" key={`${comment.id}`}>
+                                        <h4>You commented:</h4>
+                                        <p className="user-comments-card-body">"{comment.body}"</p>
+                                        <h5> <span>
+                                            On <span>{comment.post.title}</span> by <span>{comment.post.author.username}</span>
+                                            <span> at {comment.createdAt}</span>
+                                        </span> </h5>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                }}
+            </Query>
+        </div>
+    )
 }
 
 export default UserComments;
