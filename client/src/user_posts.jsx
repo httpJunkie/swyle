c49e4f32f57afac2c4afbe6275c009a105fe89b9
genxhero <genxhero@users.noreply.github.com>
2019-12-21 14:14:54 -0800

    move post info to its own component

--- client/src/user_posts.jsx
+++ client/src/user_posts.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
-const UserPosts = (props) => {
+/**
+ * Expected Props:
+ * UserId: Integer, ID of current user.
+ */
 
+const UserPosts = (props) => {
+return (
+    <div className="user-posts">
+        <h3 style={{ "textAlign": "center", "color": "white" }}> Posts</h3>
+        <Query query={postsByUser} variables={{ userId: props.userId }}>
+            {({ loading, error, data }) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return <p>Error </p>;
+                return (
+                    <div>
+                        {data.postsByUser.map(post => {
+                            return (
+                                <div className="user-posts-card" key={`${post.title}${post.id}`}>
+                                    <h3 >{post.title}</h3>
+                                    {post.image && <div className="user-posts-thumbnail-container"><img src={post.image} alt={post.image.title} /> </div>}
+                                    {post.snippet && <p>{post.snippet}<span style={{ "color": "gray" }}>...</span></p>}
+                                </div>
+                            )
+                        })}
+                    </div>
+                )
+            }}
+        </Query>
+
+    </div>
+)
 }
 
 export default UserPosts;
