3c86056098b40a1afb7caa1786d57939007e895e
genxhero <genxhero@users.noreply.github.com>
2019-12-25 19:25:19 -0800

    remove stupid changes, quill doesnt work

--- client/src/article_body_edit.jsx
+++ client/src/article_body_edit.jsx
@@ -1,11 +1,12 @@
 /**
  * Edit form for the body of an article
  */
-
+/* eslint no-restricted-globals: 0 */  // --> OFF
 import React, {Component} from 'react';
 import {Mutation} from 'react-apollo';
 import updateArticleBody from './mutations/update_article_body'
 import article from './queries/article';
+// import "react-quill/dist/quill.snow.css";
 
 class ArticleBodyEdit extends Component {
     constructor(props) {
@@ -19,54 +20,76 @@ class ArticleBodyEdit extends Component {
     }
 
     handleFormChange(field) {
-        return event => this.setState({
-            [field]: event.currentTarget.value,
-        });
+        return event =>
+          this.setState({
+             [field]: event.currentTarget.value
+            //[field]: event.target.value
+          });
     }
 
     render() {
 
         return (
-            <div className="edit-body">
-                <Mutation mutation={updateArticleBody}
-                    update={(cache, { data: { updateArticleBody } }) => {
+          <div className="edit-body">
+            <Mutation
+              mutation={updateArticleBody}
+              update={(cache, { data: { updateArticleBody } }) => {}}
+              refetchQueries={[
+                { query: article, variables: { id: this.props.id } }
+              ]}
+            >
+              {(updateArticleBody, loading) =>
+                !loading ? (
+                  "..."
+                ) : (
+                  <form
+                    className="edit-body"
+                    onSubmit={event => {
+                      event.preventDefault();
+                      updateArticleBody({
+                        variables: {
+                          body: this.state.body,
+                          id: this.props.id
+                        }
+                      }).then(res => {
+                        this.props.finishEdit("Body");
+                      });
                     }}
-                    refetchQueries={[{ query: article, variables: { id: this.props.id } }]}
-
-                >{(updateArticleBody, loading) =>
-                    !loading ? (
-                        "..."
-                    ) : (
-                            <form className="edit-body" onSubmit={event => {
-                                event.preventDefault();
-                                updateArticleBody({
-                                    variables: {
-                                        body: this.state.body,
-                                        id: this.props.id
-                                    }
-                                }).then(res => {
-                                    this.props.finishEdit("Body");
-                                })
-                            }}>
-                            <textarea className="edit-body-textarea" 
-                              type="textarea" 
-                              wrap="hard"
-                              cols="40"
-                              onChange={this.handleFormChange("body")} 
-                              value={this.state.body}
-                              style={{"height":`${this.bodyEditHeight}px`}}
-                            />
-                            <span className="edit-save-or-cancel">
-                                <input type="submit" value="Save" className="confirm-btn-yes" name="Save" />
-                                <button onClick={this.props.cancelEdit} className="confirm-btn-no" name="Body">Cancel</button>
-                            </span>
-                            </form>
-                        )}
-                </Mutation>
-
-            </div>
-        )
+                  >
+                    <textarea
+                      type="textarea"
+                      className="edit-body-textarea"
+                      wrap="hard"
+                      cols="40"
+                      onChange={this.handleFormChange("body")}
+                      value={this.state.body}
+                      style={{ height: `${this.bodyEditHeight}px` }}
+                    />
+                    <span className="edit-save-or-cancel">
+                      <input
+                        type="submit"
+                        value="Save"
+                        className="confirm-btn-yes"
+                        name="Save"
+                      />
+                      <button
+                        onClick={this.props.cancelEdit}
+                        className="confirm-btn-no"
+                      >
+                        Cancel
+                      </button>
+                    </span>
+                  </form>
+                )
+              }
+            </Mutation>
+          </div>
+        );
     }
 }
-
+//                      
 export default ArticleBodyEdit;
+
+/**
+ *  
+ */
