2c2aba94ed18ca8cdf6ff65d6d98a5355becbe16
genxhero <genxhero@users.noreply.github.com>
2020-01-30 09:04:37 -0800

    basic post card innards

--- client/src/PostCard.jsx
+++ client/src/PostCard.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const PostCard = (props) => {
-
+    const {colorScheme, post} = props;
+    return (
+        <div className={`post-card ${colorScheme}`} key={`${post.title}${post.id}`}>
+            <h3 >{post.title}</h3>
+            {post.image && <div className="post-thumbnail-container"><img src={post.image} alt={post.image.title} /> </div>}
+            {post.snippet && <p>{post.snippet}<span style={{ "color": "gray" }}>...</span></p>}
+        </div>
+    )
 }
 
 export default PostCard;
