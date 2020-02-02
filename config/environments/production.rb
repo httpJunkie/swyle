99c34b77515963a518ffa374e40232fdf64c3803
genxhero <genxhero@users.noreply.github.com>
2020-02-02 10:01:41 -0800

    change action cable allowed sources back to an array

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://afternoon-eyrie-69554.herokuapp.com/cable'
   #config.action_cable.allowed_request_origins = ['https://afternoon-eyrie-69554.herokuapp.com/' ]
-  config.action_cable.allowed_request_origins = 'https://afternoon-eyrie-69554.herokuapp.com' 
+  config.action_cable.allowed_request_origins = ['https://afternoon-eyrie-69554.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
