d95e4bb28db13115792c0e945feb76b525c16962
genxhero <genxhero@users.noreply.github.com>
2020-02-12 08:57:34 -0800

    add class names for image creation

--- client/src/image_create.jsx
+++ client/src/image_create.jsx
@@ -110,11 +110,11 @@ class ImageCreate extends Component {
            return <Redirect to="/login" />
        }
        return (
-        <div className={`image-creation-page image-creation-page-${this.props.colorScheme}`}>
+        <div className={`image-creation-page ${this.props.colorScheme}`}>
                <div className="image-input-container">
                    {this.state.image ? <div className="image-creation-preview"><img src={this.state.photoURL} alt="Preview"/></div> : <div className="image-creation-placeholder"><p>Your image will appear here in preview</p></div>}
                </div>
-            <form onSubmit={this.save} className="image-creation-form">
+            <form onSubmit={this.save} className={`image-creation-form" ${this.props.colorScheme}`}>
                    <h1>Post a New Image</h1>
                <label className="image-input">
                    <input className="image-input"
