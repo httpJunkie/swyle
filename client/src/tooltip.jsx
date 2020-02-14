c092cb506ac5a346d0bade38590782c8b2755831
genxhero <genxhero@users.noreply.github.com>
2020-02-13 16:41:49 -0800

    fuunctional component skeleton for tooltip

---
+++ client/src/tooltip.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tooltip = props => {
+    const {message, visibility} = props;
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tooltip;
